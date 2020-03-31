8619f8cebc6cfad8fcf51cf5604133c0005d0428
Mayank Padshala <mayankpadshala3012@gmail.com>
2020-03-31 13:08:30 -0700

    Updates

---
+++ precache-manifest.fc1eab295a5bb6cc97e52e9ab026aec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94344c506daa6bb28e1a3432e89d4f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d05025b37152e2a89e",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "63f5c88425092bcf8bf7",
+    "url": "/static/js/2.d04fca3c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d04fca3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d05025b37152e2a89e",
+    "url": "/static/js/main.646e838e.chunk.js"
+  },
+  {
+    "revision": "1b3adccadec16eba6cdf",
+    "url": "/static/js/runtime-main.30a0de67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/YDSCA_ORG/precache-manifest.4064f12d457689ed8456190b0b628947.js"
+  "/precache-manifest.fc1eab295a5bb6cc97e52e9ab026aec3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YDSCA_ORG/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
