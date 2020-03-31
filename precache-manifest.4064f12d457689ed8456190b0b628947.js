4b45763d96b131017177fe4cc1f76c1b94d47c17
Mayank Padshala <mayankpadshala3012@gmail.com>
2020-03-31 13:03:09 -0700

    Updates

---
+++ precache-manifest.4064f12d457689ed8456190b0b628947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da46d7ba66ee1080f2a7ca4ec13ea45",
+    "url": "/YDSCA_ORG/index.html"
+  },
+  {
+    "revision": "28d05025b37152e2a89e",
+    "url": "/YDSCA_ORG/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "63f5c88425092bcf8bf7",
+    "url": "/YDSCA_ORG/static/js/2.d04fca3c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/YDSCA_ORG/static/js/2.d04fca3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d05025b37152e2a89e",
+    "url": "/YDSCA_ORG/static/js/main.646e838e.chunk.js"
+  },
+  {
+    "revision": "b1f42dc9bfa372585a32",
+    "url": "/YDSCA_ORG/static/js/runtime-main.58839298.js"
+  }
+]);
