b730914bf4610d3b885501825a87f2bbb1add522
Mayank Padshala <mayankpadshala3012@gmail.com>
2020-06-16 14:09:04 -0700

    Made UI Responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,76 +6,77 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
-import MainBanner from './dashboard/MainBanner'
-import SubhVichar from './dashboard/SubhVichar'
-import Articles from './dashboard/Articles'
-import Footer from './dashboard/Footer'
-import UpcomingEvents from './dashboard/UpcomingEvents'
-import img from '../media/images/img.png'
+import MainBanner from './dashboard/MainBanner';
+import SubhVichar from './dashboard/SubhVichar';
+import Articles from './dashboard/Articles';
+import Footer from './dashboard/Footer';
+import UpcomingEvents from './dashboard/UpcomingEvents';
+import img from '../media/images/img.png';
 
 const useStyles = makeStyles((theme) => ({
-    mainGrid: {
-        marginTop: "36px",
-    },
-    root: {
-        backgroundColor: "#fff7ea",
-        height: "100%",
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        height: "100%",
-        color: theme.palette.text.secondary,
-    }
+  mainGrid: {
+    marginTop: '36px',
+  },
+  root: {
+    backgroundColor: '#fff7ea',
+    height: '100%',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    height: '100%',
+    color: theme.palette.text.secondary,
+  },
 }));
 
 const mainBannerProps = {
-    img: 'https://source.unsplash.com/random',
-    image: require("../media/images/HaridhamMandir.jpg"),
+  img: 'https://source.unsplash.com/random',
+  image: require('../media/images/HaridhamMandir.jpg'),
 };
 
 const Home = (props) => {
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    return (
-        <Fragment>
-            <Paper className={classes.mainGrid}>
-                <CssBaseline />
-                <Grid container spacing={0} elevation={0}>
-                    <Grid item xs={12} elevation={0} className={classes.mainGrid}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <MainBanner mainBanner={mainBannerProps} />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} elevation={0}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <SubhVichar mainBanner={mainBannerProps} />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <Articles atrticles={mainBannerProps} />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <div className={classes.root}>
-                                <UpcomingEvents />
-                            </div>
-                        </Paper>
-                    </Grid>
-                </Grid>
-                <Footer description={"Description"} title={"Yogi Divine Society - California"} />
+  return (
+    <Fragment>
+      <Paper className={classes.mainGrid}>
+        <CssBaseline />
+        <Grid container spacing={0} elevation={0}>
+          <Grid item xs={12} elevation={0} className={classes.mainGrid}>
+            <Paper className={classes.paper} elevation={0}>
+              <MainBanner mainBanner={mainBannerProps} />
             </Paper>
-        </Fragment >
-    );
-}
-
+          </Grid>
+          <Grid item xs={12} elevation={0}>
+            <Paper className={classes.paper} elevation={0}>
+              <SubhVichar mainBanner={mainBannerProps} />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper} elevation={0}>
+              <Articles atrticles={mainBannerProps} />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Paper className={classes.paper} elevation={0}>
+              <div className={classes.root}>
+                <UpcomingEvents />
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Footer
+          description={'Description'}
+          title={'Yogi Divine Society - California'}
+        />
+      </Paper>
+    </Fragment>
+  );
+};
 
 Home.propTypes = {
-    mainBanner: PropTypes.object,
-    atrticles: PropTypes.object,
+  mainBanner: PropTypes.object,
+  atrticles: PropTypes.object,
 };
 
 export default Home;
--- src/components/dashboard/SubhVichar.js
+++ src/components/dashboard/SubhVichar.js
@@ -7,82 +7,79 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import MainBanner from './MainBanner'
+import MainBanner from './MainBanner';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        flexWrap: "wrap",
-        backgroundColor: "#fff7ea"
+  root: {
+    flexGrow: 1,
+    flexWrap: 'wrap',
+    backgroundColor: '#fff7ea',
+  },
+  heading: {},
+  paper: {
+    backgroundColor: '#fff7ea',
+  },
+  description: {
+    marginTop: 8,
+    marginLeft: 16,
+  },
+  mainFeaturedPostContent: {
+    position: 'relative',
+    marginRight: 0,
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(4),
+      paddingRight: 0,
     },
-    heading: {
-
-    },
-    paper: {
-        backgroundColor: "#fff7ea"
-    },
-    description: {
-        marginTop: 8,
-        marginLeft: 16,
-    },
-    mainFeaturedPostContent: {
-        position: 'relative',
-        marginRight: 0,
-        [theme.breakpoints.up('md')]: {
-            padding: theme.spacing(4),
-            paddingRight: 0,
-        },
-    },
-    backgroundCream: {
-        backgroundColor: "#fff7ea"
-    }
+  },
+  backgroundCream: {
+    backgroundColor: '#fff7ea',
+  },
 }));
 
 const SubhVichar = (props) => {
+  const classes = useStyles();
+  const { mainBanner } = props;
 
-    const classes = useStyles();
-    const { mainBanner } = props;
-
-    return (
-
-        <Grid container elevation={0} className={classes.root}>
-            <Grid item xs={8} elevation={0} className={classes.text}>
-                <Paper className={classes.paper} elevation={0}>
-                    <div className={classes.mainFeaturedPostContent}>
-                        <Typography className={classes.heading}
-                            component="h1"
-                            variant="h4"
-                            color="inherit"
-                            gutterBottom
-                        >
-                            Why do I need “A Saint” in my life.
-                        </Typography>
-                        <Divider variant="middle" />
-                        <Grid item xs={12} elevation={0} className={classes.description}>
-                            <Divider orientation="vertical" flexItem />
-                            <Typography variant="h6" color="inherit" paragraph align="left">
-                                A saint is true friend of soul.
-                                The selfless love from the saint is capable to uplift our soul and channelize the potential within us.
-                                <Link variant="subtitle1" href="#">
-                                    Read Mode
-                                </Link>
-                            </Typography>
-                        </Grid>
-                    </div>
-                </Paper>
+  return (
+    <Grid container elevation={0} className={classes.root}>
+      <Grid item sm={8} xs={12} elevation={0} className={classes.text}>
+        <Paper className={classes.paper} elevation={0}>
+          <div className={classes.mainFeaturedPostContent}>
+            <Typography
+              className={classes.heading}
+              component='h1'
+              variant='h4'
+              color='inherit'
+              gutterBottom
+            >
+              Why do I need “A Saint” in my life.
+            </Typography>
+            <Divider variant='middle' />
+            <Grid item xs={12} elevation={0} className={classes.description}>
+              <Divider orientation='vertical' flexItem />
+              <Typography variant='h6' color='inherit' paragraph align='left'>
+                A saint is true friend of soul. The selfless love from the saint
+                is capable to uplift our soul and channelize the potential
+                within us.
+                <Link variant='subtitle1' href='#'>
+                  Read Mode
+                </Link>
+              </Typography>
             </Grid>
-            <Grid item xs={4} elevation={0}>
-                <Paper className={classes.paper} elevation={0}>
-                    <MainBanner mainBanner={mainBanner} />
-                </Paper>
-            </Grid>
-        </Grid >
-    );
-}
-
+          </div>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={4} elevation={0}>
+        <Paper className={classes.paper} elevation={0}>
+          <MainBanner mainBanner={mainBanner} />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
 
 SubhVichar.propTypes = {
-    mainBanner: PropTypes.object,
+  mainBanner: PropTypes.object,
 };
 
 export default SubhVichar;
